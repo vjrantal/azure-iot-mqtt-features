c277530742d7bca7543076bc65af29d53579c94d
Eliise S <elseling@microsoft.com>
2020-10-07 10:24:26 +0000

    Add message sample

---
+++ MessageSample/Program.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.Azure.Devices.Client;
+
+namespace MessageSample
+{
+    public class Program
+    {
+        // String containing Hostname, Device Id, Module Id & Device Key in one of the following formats:
+        //  "HostName=<iothub_host_name>;DeviceId=<device_id>;ModuleId=<module_id>;SharedAccessKey=<device_key>"
+        //  "HostName=<iothub_host_name>;CredentialType=SharedAccessSignature;DeviceId=<device_id>;ModuleId=<module_id>;SharedAccessSignature=SharedAccessSignature sr=<iot_host>/devices/<device_id>&sig=<token>&se=<expiry_time>";
+        // For this sample either
+        // - pass this value as a command-prompt argument
+        // - set the IOTHUB_MODULE_CONN_STRING environment variable 
+        // - create a launchSettings.json (see launchSettings.json.template) containing the variable
+        private static string connectionString = Environment.GetEnvironmentVariable("IOTHUB_MODULE_CONN_STRING");
+
+        public static int Main(string[] args)
+        {
+            var simDeviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
+
+
+            var sample = new TelemetryMessage(simDeviceClient);
+            sample.RunSampleAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Done.\n");
+            return 0;
+        }
+    }
+}
