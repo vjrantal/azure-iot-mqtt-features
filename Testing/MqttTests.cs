2c33a123742fd28f3969e02a40cdda8d13569abf
Eliise S <elseling@microsoft.com>
2020-10-26 17:41:39 +0000

    Merge

--- Testing/MqttTests.cs
+++ Testing/MqttTests.cs
@@ -3,15 +3,15 @@ using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Client;
 using CrossCutting;
 using IotHubConsumer;
-using MessageSample;
 using MQTTnet;
 using NUnit.Framework;
 
 namespace Testing
 {
-    public class C2DTests
+    public class MqttTests
     {
         private string iotHubConnectionString;
         private string iotHubDeviceConnectionString;
@@ -19,7 +19,6 @@ namespace Testing
         private string eventHubCompatibleEndpoint;
         private string eventHubName;
         private string iotHubSasKey;
-
 
         [SetUp]
         public void Setup()
@@ -126,6 +125,27 @@ namespace Testing
             // Assert
             var sentMessage = messages.FirstOrDefault(x => x.Payload == payload);
             Assert.IsTrue(sentMessage != null);
+        }
+
+        [Test]
+        public async Task SendWillMessageWhenDeviceDisconnectsUngracefully()
+        {
+            // Arrange
+            var willPayload = Guid.NewGuid().ToString();
+            var receiver = new Receiver(eventHubCompatibleEndpoint, eventHubName, iotHubSasKey);
+            var device = new Device(iotHubDeviceConnectionString);
+
+            // Act
+            await device.ConnectDevice(willPayload);
+            device.DisconnectUngracefully();
+
+            var cancellationSource = new CancellationTokenSource();
+            cancellationSource.CancelAfter(3000);
+            var messages = await receiver.ReceiveMessagesFromDeviceAsync(cancellationSource.Token);
+
+            // Assert
+            var sentMessage = messages.FirstOrDefault(x => x.Payload == "WILL message " + willPayload);
+            Assert.IsTrue(sentMessage != null && sentMessage.RetainFlag == "true" && sentMessage.MessageType == "Will");
         }
 
         private bool RetryUntilSuccessOrTimeout(Func<bool> task, TimeSpan timeSpan)
