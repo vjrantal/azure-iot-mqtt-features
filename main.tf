55848c8f196b48e5d68c9bf69944cbcb4611a914
Eliise S <elseling@microsoft.com>
2020-10-26 17:36:58 +0000

    Add custom event hub and routing

--- main.tf
+++ main.tf
@@ -1,6 +1,29 @@
 resource "azurerm_resource_group" "mqtt_iot" {
-  name     = format("%s%s", var.environment_id, "_mqtt_iot")
+  name     = format("%s_mqtt_iot", var.environment_id)
   location = "West Europe"
+}
+
+resource "azurerm_eventhub_namespace" "mqtt_iot" {
+  name                = format("%s-custom-ns", var.environment_id)
+  resource_group_name = azurerm_resource_group.mqtt_iot.name
+  location            = azurerm_resource_group.mqtt_iot.location
+  sku                 = "Basic"
+}
+
+resource "azurerm_eventhub" "mqtt_iot" {
+  name                = format("%s_custom_eventhub", var.environment_id)
+  resource_group_name = azurerm_resource_group.mqtt_iot.name
+  namespace_name      = azurerm_eventhub_namespace.mqtt_iot.name
+  partition_count     = 2
+  message_retention   = 1
+}
+
+resource "azurerm_eventhub_authorization_rule" "mqtt_iot" {
+  resource_group_name = azurerm_resource_group.mqtt_iot.name
+  namespace_name      = azurerm_eventhub_namespace.mqtt_iot.name
+  eventhub_name       = azurerm_eventhub.mqtt_iot.name
+  name                = "acctest"
+  send                = true
 }
 
 resource "azurerm_iothub" "mqtt_iot" {
@@ -10,8 +33,22 @@ resource "azurerm_iothub" "mqtt_iot" {
 
   # Define the Pricing Tier (SKU & Capacity)
   sku {
-      name     = "S1"
-      capacity = 1
+    name     = "S1"
+    capacity = 1
+  }
+
+  endpoint {
+    type              = "AzureIotHub.EventHub"
+    connection_string = azurerm_eventhub_authorization_rule.mqtt_iot.primary_connection_string
+    name              = "customevents"
+  }
+
+  route {
+    name           = "customroute"
+    source         = "DeviceMessages"
+    condition      = "true"
+    endpoint_names = ["customevents"]
+    enabled        = true
   }
 
   route {
--- makefile
+++ makefile
@@ -15,11 +15,20 @@ get-iot-hub-conn:
 get-iot-hub-sas-key:
 	az iot hub policy show --name service --query primaryKey --hub-name $$(terraform output iothub_name)
 
+get-event-hub-name:
+	az iot hub show --query properties.eventHubEndpoints.events.path --name $$(terraform output iothub_name)
+
 get-event-hub-endpoint:
 	az iot hub show --query properties.eventHubEndpoints.events.endpoint --name $$(terraform output iothub_name)
 
-get-event-hub-name:
-	az iot hub show --query properties.eventHubEndpoints.events.path --name $$(terraform output iothub_name)
+get-custom-event-hub-name:
+	terraform show -json | jq -r '.values.root_module.resources[] | select(.address=="azurerm_eventhub.mqtt_iot") | .values.name'
+
+get-custom-event-hub-endpoint:
+	namespace=$$(terraform show -json | jq -r '.values.root_module.resources[] | select(.address=="azurerm_eventhub_namespace.mqtt_iot") | .values.name') && \
+	rg=$$(terraform show -json | jq -r '.values.root_module.resources[] | select(.address=="azurerm_resource_group.mqtt_iot") | .values.name') && \
+	az eventhubs namespace authorization-rule keys list --resource-group $$rg --namespace-name $$namespace --name RootManageSharedAccessKey \
+	| jq -r '.primaryConnectionString'
 
 lint: md-lint md-spell-check md-link-check dotnet-lint
 
